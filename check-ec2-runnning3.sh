96f040c48bd6f098136b6f00feb065220d461d0a
miyajuggler <b.m.juggler@gmail.com>
2021-12-31 17:33:09 +0900

    [update]check-ec2-running3

--- check-ec2-runnning3.sh
+++ check-ec2-runnning3.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 # インスタンスが起動しているのかどうかチェックjqコマンドバージョン
-# 構造はかなり似ている。 -cで
+# 構造はかなり似ている。 -cでjson形式ではなくす。
 aws ec2 describe-instances \
 | jq -c '.Reservations[].Instances[] | {Name: .Tags[] | select(.Key == "Name").Value, Id: .InstanceId, State: .State.Name}'
