4d0871841feb87d518f5e5a84758a5d1f23a221e
miyajuggler <b.m.juggler@gmail.com>
2021-12-31 16:57:16 +0900

    [update]check-ec2-runnning.sh

--- check-ec2-runnning.sh
+++ check-ec2-runnning.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 
 # インスタンスが起動しているのかどうかチェック
 # []でも[*]でもどちらでも大丈夫
+# Name:Tags[?Key==\`Name\`]|[0].Value ここでtagのKeyがNameになっているものだけを取得。
+# 階層が違うので|[0]で下に持ってこれる。
 aws ec2 describe-instances \
     --query "Reservations[].Instances[].{
     Name:Tags[?Key==\`Name\`]|[0].Value,
---
+++ check-ec2-runnning2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euo pipefail
+
+# インスタンスが起動しているのかどうかチェックCSVバージョン
+# 一番下のやつをするだけ。意味はわからん
+# 前半の jq '.[]'で階層をひとつ上に持ってきている。
+aws ec2 describe-instances \
+    --query "Reservations[].Instances[].{
+    Name:Tags[?Key==\`Name\`]|[0].Value,
+    Id:InstanceId,
+    Status:State.Name
+}" \
+|  jq '.[]' | jq -rs '(.[0]|keys_unsorted),map([.[]])[]|@csv'
