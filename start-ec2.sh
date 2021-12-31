1a0acd0c696199298f153a784faa10f60b39e188
miyajuggler <b.m.juggler@gmail.com>
2021-12-31 18:21:18 +0900

    [update]変数格納

--- start-ec2.sh
+++ start-ec2.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+ID="i-066b058a880601015"
+
 # 特定のインスタンス起動からのチェック
-aws ec2 start-instances --instance-ids i-066b058a880601015 \
+aws ec2 start-instances --instance-ids $ID \
 && echo "-- Please wait until instance started..." \
-&& aws ec2 wait instance-running --instance-ids i-066b058a880601015 \
-&& aws ec2 describe-instance-status --instance-ids i-066b058a880601015 \
+&& aws ec2 wait instance-running --instance-ids $ID \
+&& aws ec2 describe-instance-status --instance-ids $ID \
 | jq '.InstanceStatuses[] | {InstanceId, InstanceState: .InstanceState.Name, SystemStatus: .SystemStatus.Status, InstanceStatus: .InstanceStatus.Status}'
