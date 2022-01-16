3cc8764764a6f53ca091573e07eff9473e31f32d
miyajuggler <b.m.juggler@gmail.com>
2022-01-16 17:42:24 +0900

    [create]start-ec2-all.sh

--- start-ec2-all.sh
+++ start-ec2-all.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 # 停止しているインスタンスをすべて起動。
-aws ec2 start-instances --instance-ids $(aws ec2 describe-instances
+aws ec2 start-instances --instance-ids $(aws ec2 describe-instances \
 | jq -r '[.Reservations[].Instances[] | select(.State.Name == "stopped") | .InstanceId] | join(" ")')
