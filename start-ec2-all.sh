953cbc513df189b4bede0a4347907bf087a54657
miyajuggler <b.m.juggler@gmail.com>
2022-01-16 17:51:32 +0900

    [update]コメント追加

--- start-ec2-all.sh
+++ start-ec2-all.sh
@@ -2,5 +2,7 @@
 set -euo pipefail
 
 # 停止しているインスタンスをすべて起動
+# join(" ")で配列とダブルクォートを消してくっつけているが、そもそも以下でよし
+# | jq -r '[.Reservations[].Instances[] | select(.State.Name == "stopped") | .InstanceId
 aws ec2 start-instances --instance-ids $(aws ec2 describe-instances \
 | jq -r '[.Reservations[].Instances[] | select(.State.Name == "stopped") | .InstanceId] | join(" ")')
--- stop-ec2-all.sh
+++ stop-ec2-all.sh
@@ -2,5 +2,7 @@
 set -euo pipefail
 
 # インスタンス全停止
+# join(" ")で配列とダブルクォートを消してくっつけているが、そもそも以下でよし
+# | jq -r '[.Reservations[].Instances[] | select(.State.Name == "running") | .InstanceId
 aws ec2 stop-instances --instance-ids $(aws ec2 describe-instances \
 | jq -r '[.Reservations[].Instances[] | select(.State.Name == "running") | .InstanceId] | join(" ")')
